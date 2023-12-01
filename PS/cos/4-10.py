eca34b7630ef6e17e718342b033f9966f3ff4b70
jyzayu <moosou30@gmail.com>
2023-12-01 16:39:18 +0900

    Create 4-10.py

---
+++ PS/cos/4-10.py
@@ -0,0 +1,29 @@
+# 에라토스테네스   그냥 소수 구하는거 많이 해야될 떄니까 발상이 쉽네  그래도 늘긴 하는구나
+def solution(a, b):
+    # 여기에 코드를 작성해주세요.
+    #b//2 까지의 소수를 모두 구한다? 에라토스테네스?
+    #2, 3 각 수(isPrime=false인 것 중 가장 작은수) 의 *2부터 b//2까지 배수를 모두 지운다. 
+    # isPrime[i] = false
+    n = b // 2
+    answer = 0
+
+    isPrime = [True] * (n + 1)
+    isPrime[1] = False
+    for i in range(2, n + 1):
+        if isPrime[i] == True:
+            j = 2
+            while i * j < n + 1:
+                isPrime[i * j] = False
+                j += 1
+    print(isPrime)
+#2 3 5 7 11 13
+    #isPrime == True인 것들을 모두 소수 리스트에 
+
+    for i in range(2, n + 1):
+        if isPrime[i] == True:
+            if a <= pow(i, 2) <= b:
+                answer += 1
+            if a <= pow(i, 3) <= b:
+                answer += 1
+    #그리고 그 소수들을 모두 제곱 세제곱해서 범위에 속하는지 확인? a < b
+    return answer
